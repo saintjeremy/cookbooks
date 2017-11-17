d8cb58307b9785e2b17a4f9a0f4a8309875c4b04
Jeremy Stout <saintjeremy@users.noreply.github.com>
2017-11-17 10:08:08 -0800

    default site admin for guyona domains

---
+++ recipes/lamp/attrib/default.rb
@@ -0,0 +1,4 @@
+ ## configuration for guyona.com .co domains. 
+
+default["lamp-stack"]["sites"]["guyona.com"] = { "port" => 80, "servername" => "guyona.com", "serveradmin" => "webmaster@guyona.com" }
+default["lamp-stack"]["sites"]["guyona.co"] = { "port" => 80, "servername" => "guyona.co", "serveradmin" => "webmaster@guyona.co" }
